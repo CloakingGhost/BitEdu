8476ce9cf279c1a892266d1878b91bbc44b42f14
JaeHyeon Lee <project0858@gmail.com>
2022-08-16 12:36:49 +0900

    commit

--- study/src/study/chap01/exam01/Class.java
+++ study/src/study/chap01/exam01/Class.java
@@ -4,17 +4,30 @@ public class Class {
 
 	public static void main(String[] args) {
 		System.out.print("Hello");
-		root
-		1234
-
-		scott
-		tiger
-
-		hr
-		hr
-
-		CloackingGhost	cj92978364
-		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(깃허브 이클립스 토큰)
+//		root
+//		1234
+//
+//		scott
+//		tiger
+//
+//		hr
+//		hr
+//
+//		CloackingGhost	cj92978364
+//		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(깃허브 이클립스 토큰)
+		/*
+		  잘하는 사람 주변으로 가라
+		  구글링 잘하는 사람 
+		  가감없이 복붙하는 사람
+		  왜라는 질문
+		  타자속도가 나와야함
+		  지원동기를 잊지마라
+		  
+		  시험 목요일 점심이후 JDBC까지(파일 네트워크 시험끝나고 배움)
+		  
+		  
+		  
+		 */
 	}
 
 }
